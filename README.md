610e3fbfedc5bf6d4e038bf5d8569f7837f8c9e4
castelanlan <gabrielmcastelan@gmail.com>
2022-12-25 12:31:58 -0300

    small code refactor, read me

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## Site pessoal
-Site para divulgar minhas fotografias
-Feito por Gabriel Castelan
+Site para divulgar minhas fotografias, feito por Gabriel Castelan.
+
+### Metas
+- [x] Página principal
+- [ ] Páginas dinâmicas para cada fotografia
+- [ ] Domínio & Hosting
+- [ ] Lançamento :pray:
--- script.js
+++ script.js
@@ -8,15 +8,6 @@ var clicks = 0
 var pageBottom = document.getElementById("header")
 const title = document.getElementById('title')
 
-// title.onclick = function () {
-//     clicks++
-//     title.innerHTML = synms.random()
-//     if (clicks > 2) {
-//         // window.scroll(0, window.innerHeight)
-//     }
-// }
-
-
 title.addEventListener("click", function () {
     title.innerHTML = synms.random()
     clicks++    
