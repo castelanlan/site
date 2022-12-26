3b843be408422e8a4dd5c0283f3a92a3a4f69bb4
castelanlan <gabrielmcastelan@gmail.com>
2022-12-26 11:05:47 -0300

    Things in a functional state +/-

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
+var synms = ['castelan', 'stylish', 'beautiful', 'chique', 'elegant', 'fash', 'classy', 'dapper', 'dressy', 'fashionable', 'sleek', 'sharp', 'urban']
+var clicks = 0
+var pageBottom = document.getElementById("header")
+var left = document.getElementById("header-p")
+
+// [].random()
 Array.prototype.random = function () {
     return this[Math.floor((Math.random() * this.length))];
 }
 
-var synms = ['stylish', 'beautiful', 'chique', 'elegant', 'fash', 'classy', 'dapper', 'dressy', 'fashionable', 'sleek', 'sharp', 'urban']
-
-var clicks = 0
-var pageBottom = document.getElementById("header")
 const title = document.getElementById('title')
-
 title.addEventListener("click", function () {
     title.innerHTML = synms.random()
-    clicks++    
+    clicks++
     if (clicks == 3) {
         pageBottom.scrollIntoView()
-    } 
+    }
+})
+
+var i = 0
+left.addEventListener('click', function () {
+    i++;
+    if (i % 2 == 0){
+        left.innerText = 'Castelan'
+    } else {
+        left.innerText = 'Cstln'
+    }
 })
