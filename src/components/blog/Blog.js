895edeb1c1ff431fd95adbd5cb3a5d3e20b4f18b
castelanlan <gabrielmcastelan@gmail.com>
2024-04-08 13:41:24 -0300

    Pasta blogs, renderização dos blogs

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,11 +1,16 @@
 import React from "react";
-import Header from "../Header";
+import Header, { BlogHeader } from "../Header";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import Markdown from "markdown-to-jsx";
+import tecnologia from "../../static/posts/tecnologia.md"
 
 import './blog.css'
 
 function Blog() {
   const { title } = useParams();
+  const [texto, setTexto] = useState('');
+  
   if (title == 0) {
     return (
       <div className="blog">
@@ -14,7 +19,21 @@ function Blog() {
       </div>
     );
   } else if (title === 'titulo-1') {
-    return (<><p>Título 1</p></>)
+    fetch(tecnologia).then(res => res.text()).then(text => setTexto(text));
+
+    return (<><Markdown
+                options={{
+                  overrides: {
+                    h1: {
+                      component: BlogHeader,
+                      props: {
+                        className: 'header',
+                        title: 'Título'
+                      },
+                    },
+                  },
+                }}
+    >{ texto }</Markdown></>);
   } else if (title === 'titulo-2') {
     return (<><p>Título 2</p></>)
   } else if (title === 'titulo-3') {
