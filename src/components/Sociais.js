cf73bcd1d1bee2c9d650ae4f81158bb83347ce09
castelanlan <gabrielmcastelan@gmail.com>
2024-04-08 13:51:07 -0300

    github link e alerta security

--- src/components/Sociais.js
+++ src/components/Sociais.js
@@ -14,6 +14,7 @@ function Sociais() {
       <div className="sociais-links">
         <p><a className="link" href="https://www.instagram.com/gabrielmcastelan/">Instagram @gabrielmcastelan</a></p>
         <p><a className="link" href="https://twitter.com/castelanches">Twitter @castelanches</a></p>
+        <p><a className="link" href="https://github.com/castelanlan">Github /castelanlan</a></p>
         <p><a className="link" href="mailto:castelan@castelan.art.br">E-mail: castelan@castelan.art.br</a></p>
         <p><a className="link" href="mailto:gabriel@castelan.art.br">E-mail: gabriel@castelan.art.br</a></p>
       </div>
